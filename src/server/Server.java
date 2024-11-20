df3bf43df85a0a968d13f92053f0db97ae6e8604
Isak Folke <isvack@gmail.com>
2024-11-20 14:07:08 +0100

    Unfinished

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,6 +11,8 @@ public class Server implements Runnable {
 
     Socket[] clientSockets = new Socket[2];
     ServerProtocol[] serverProtocols = new ServerProtocol[2];
+    ObjectInputStream[] in;
+    ObjectOutputStream[] out;
 
     private void startServer() {
         try {
@@ -19,6 +23,8 @@ public class Server implements Runnable {
                     clientSocket = serverSocket.accept();
 
                     clientSockets[clientIndex] = clientSocket;
+                    in[clientIndex] = new ObjectInputStream(clientSocket.getInputStream());
+                    out[clientIndex] = new ObjectOutputStream(clientSocket.getOutputStream());
                     System.out.println("Client " + clientIndex + " connected");
                     ServerProtocol protocol = new ServerProtocol(clientSocket);
                     serverProtocols[clientIndex] = protocol;
@@ -36,7 +42,25 @@ public class Server implements Runnable {
     }
 
     private void handleClients() {
+        GameState gameState = new GameState(1, 2);
+        int currentClient = 0;
 
+        while (true) {
+            try {
+                Object o;
+                while ((o = in[currentClient].readObject()) != null) {
+                    out[currentClient].writeObject(serverProtocols[currentClient].processInput(o, currentClient, gameState));
+                    System.out.println("Sent:  " + o + " to client: " + currentClient);
+                    if (o instanceof GameState) {
+                        currentClient = (currentClient + 1) % 2; //nextClient()
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                System.err.println("Error reading object: " + e.getMessage());
+            } catch (IOException e) {
+                System.err.println("Error writing to server: " + e.getMessage());
+            }
+        }
     }
 
     @Override
--- src/server/ServerProtocol.java
+++ src/server/ServerProtocol.java
@@ -16,10 +16,10 @@ public class ServerProtocol implements Runnable {
     private static final int CHOOSE_CATEGORY = 1;
     private static final int PLAY_ROUND = 2;
     private static final int SHOW_RESULTS = 3;
+    private static final int ANSWER_QUESTION = 4;
 
     private int state = WAITING;
     private Categories categories = new Categories();
-    private GameState gameState = new GameState(1, 2);
     private List<Question> currentQuestions = new ArrayList<>(2);
 
     private Socket socket;
@@ -29,12 +29,11 @@ public class ServerProtocol implements Runnable {
     }
 
     @SuppressWarnings("unchecked")
-    public Object processInput(Object input, int player) {
+    public Object processInput(Object input, int player, GameState gameState) {
         Object output = null;
-        int category = 0;
 
         if (state == WAITING) {
-            output = gameState.getResults();
+            output = null; //TODO ett intro objekt
             state = CHOOSE_CATEGORY;
         } else if (state == CHOOSE_CATEGORY) {
             output = categories.getCategory(Integer.parseInt((String)input));
@@ -51,6 +50,10 @@ public class ServerProtocol implements Runnable {
 
             output = gameState.getResults();
             state = WAITING;
+        } else if (state == ANSWER_QUESTION) {
+            output = currentQuestions;
+
+            state = CHOOSE_CATEGORY;
         }
 
         return output;
@@ -58,19 +61,7 @@ public class ServerProtocol implements Runnable {
 
     @Override
     public void run() {
-        try {
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            Object o;
-            while ((o = in.readObject()) != null) {
-                out.writeObject(processInput(o));
-                System.out.println("Input processed.");
-            }
-        } catch (IOException e) {
-            System.err.println(e.getMessage() + "fel i serverprotocol");
-        } catch (ClassNotFoundException e) {
-            System.err.println("Unexpected type: " + e.getMessage());
-        }
+
     }
 
 }
