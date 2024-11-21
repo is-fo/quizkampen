97c70b4f86d2644a7980626f1a876ef71c21c155
Isak Folke <isvack@gmail.com>
2024-11-21 10:16:23 +0100

    ServerProtocol logic updated.

--- src/server/ServerProtocol.java
+++ src/server/ServerProtocol.java
@@ -5,13 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServerProtocol implements Runnable {
+    
+    private static final int INTRO = -1;
     private static final int WAITING = 0;
     private static final int CHOOSE_CATEGORY = 1;
+    private static final int CATEGORY_CHOSEN = 11;
     private static final int PLAY_ROUND = 2;
     private static final int SHOW_RESULTS = 3;
     private static final int ANSWER_QUESTION = 4;
 
-    private int state = WAITING;
+    private int state = INTRO;
     private Categories categories = new Categories();
     private List<Question> currentQuestions = new ArrayList<>(2);
 
@@ -24,18 +27,29 @@ public class ServerProtocol implements Runnable {
     @SuppressWarnings("unchecked")
     public Object processInput(Object input, int player, GameState gameState) {
         Object output = null;
-
+        int currentCategory = -1;
+        
         if (state == WAITING) {
-            output = null; //TODO ett intro objekt
-            state = CHOOSE_CATEGORY;
+            output = new Waiting();
+            if (player == 0 && gameState.getCurrentRound() == 0) {
+                state = CHOOSE_CATEGORY;
+            } else {
+                state = ANSWER_QUESTION;
+            }
         } else if (state == CHOOSE_CATEGORY) {
-            output = categories.getCategory(Integer.parseInt((String)input));
+            //TODO skicka flera kategorier
+            output = categories.getCategoryString(0);
+            state = CATEGORY_CHOSEN;
+        } else if (state == CATEGORY_CHOSEN) {
+            currentCategory = categories.getCategoryInt((String)input);
+            output = categories.getCategory(currentCategory);
             if (output != null) {
                 categories.setCurrentCategory((List<Question>) output);
             } else System.exit(30);
+            gameState.incrementRound();
             state = PLAY_ROUND;
         } else if (state == PLAY_ROUND) {
-            currentQuestions = categories.getNQuestions(2, Integer.parseInt((String)input));
+            currentQuestions = categories.getNQuestions(2, currentCategory);
             output = currentQuestions;
             state = SHOW_RESULTS;
         } else if (state == SHOW_RESULTS) {
@@ -45,6 +59,12 @@ public class ServerProtocol implements Runnable {
             state = WAITING;
         } else if (state == ANSWER_QUESTION) {
             output = currentQuestions;
+            
+//            if (player == 0 && gameState.getCurrentRound() % 2 == 0) {
+//                gameState.incrementRound();
+//            } else if (player == 1 && gameState.getCurrentRound() % 2 == 1) {
+//                gameState.incrementRound();
+//            }
 
             state = CHOOSE_CATEGORY;
         }
