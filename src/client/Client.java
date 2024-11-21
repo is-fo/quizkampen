c55f49c75e71a4534f5354590bcfaf4b3cbe910b
Jesper Ekstedt <jemaek@gmail.com>
2024-11-21 13:33:32 +0100

    Fix

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,16 @@
 package client;
 
+import server.GameState;
 import server.Intro;
+import server.Question;
+import server.Waiting;
 
+import java.io.EOFException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.List;
 
 public class Client {
 
@@ -12,7 +18,7 @@ public class Client {
         String hostName = "172.20.206.165";
         int portNumber = 55555;
 
-        try (
+       /* try (
             Socket addressSocket = new Socket(hostName, portNumber);
             ObjectOutputStream oos = new ObjectOutputStream(addressSocket.getOutputStream());
             ObjectInputStream ois = new ObjectInputStream(addressSocket.getInputStream());) {
@@ -23,8 +29,15 @@ public class Client {
                 if (fromServer instanceof Intro) {
                     System.out.println("Anslutning upprättad ");
                     oos.writeObject(new Connected());
+                    oos.writeObject(fromServer);
+                } if (fromServer instanceof Waiting) {
+                    oos.writeObject(fromServer); wait();
+                } else if (fromServer instanceof List<Question>) {
+                    //visa frågorna
+                    //skicka tillbaka svaren
+                } else if (fromServer instanceof GameState) {
+                    //Visa resultatskärmen och vänta
                 }
-
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -33,5 +46,40 @@ public class Client {
 
     public static void main(String[] args) {
         Client client = new Client();
+    }*/
+
+        try (
+                Socket addressSocket = new Socket(hostName, portNumber);
+                ObjectOutputStream oos = new ObjectOutputStream(addressSocket.getOutputStream());
+                ObjectInputStream ois = new ObjectInputStream(addressSocket.getInputStream())
+        ) {
+            while (true) {
+                Object fromServer = ois.readObject();
+                System.out.println("hej");
+
+                if (fromServer instanceof Intro) {
+                    System.out.println("Anslutning upprättad ");
+                    oos.writeObject(new Connected());
+                    oos.writeObject(fromServer);
+                } else if (fromServer instanceof Waiting) {
+                    System.out.println("Tjena");
+                        wait();
+                } else if (fromServer instanceof List<Question>) {
+                    System.out.println("Han är lugn");
+                }else if (fromServer instanceof GameState) {
+                    System.out.println("Spelstatus");
+            }
+            }
+        } catch( EOFException e){
+            System.out.println("Servern stängde anslutningen.");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        }
+        public static void main(String[] args) {
+        Client client = new Client();
     }
 }
+
+
