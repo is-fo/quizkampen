720e198644d6c405e92bc3a77791dd687cdcc7f0
Isak Folke <isvack@gmail.com>
2024-11-27 14:38:59 +0100

    Merge conflict cleanup

--- src/client/Client.java
+++ src/client/Client.java
@@ -39,47 +39,37 @@ public class Client {
                     resultPanel = new ResultPanel(i.getGameState(), oos, i.getRoundsPerGame());
                     resultPanel.createWindow();
                 } else if (fromServer instanceof List<?> receivedList) {
-                    if (!receivedList.isEmpty() && receivedList.getFirst() instanceof Question) {
-
-                } else if (fromServer instanceof String) {
-                    System.out.println(fromServer + "<- category received");
-                    oos.writeObject("Sport");
-                } else if (fromServer instanceof List<?>) {
                     resultPanel.enablePlayButton();
-                    List<?> receivedList = (List<?>) fromServer;
                     if (!receivedList.isEmpty() && receivedList.getFirst() instanceof Question q) {
                         QuestionPanel qp = new QuestionPanel((List<Question>) receivedList, oos);
                         qp.drawAll();
-                    }
-                    else if (!receivedList.isEmpty() && receivedList.getFirst() instanceof String) {
+                    } else if (!receivedList.isEmpty() && receivedList.getFirst() instanceof String) {
                         //resultPanel.enablePlayButton();
-                        CategoryPanel cp = new CategoryPanel((List<String>)fromServer, oos);
+                        CategoryPanel cp = new CategoryPanel((List<String>) fromServer, oos);
                         cp.drawCategories();
                     }
-                } else if (fromServer instanceof GameState g) {
-                    assert resultPanel != null;
-                    resultPanel.updateWindow(g.getPlayerScores());
-                } else if (fromServer instanceof Waiting) {
-                    resultPanel.disablePlayButton();
-                    oos.writeObject(fromServer);
-                } else if (fromServer instanceof EndGame) {
-                    resultPanel.disablePlayButton();
-                    oos.close();
-                    ois.close();
-                    addressSocket.close();
-                    break;
+                    } else if (fromServer instanceof GameState g) {
+                        assert resultPanel != null;
+                        resultPanel.updateWindow(g.getPlayerScores());
+                    } else if (fromServer instanceof Waiting) {
+                        resultPanel.disablePlayButton();
+                        oos.writeObject(fromServer);
+                    } else if (fromServer instanceof EndGame) {
+                        resultPanel.disablePlayButton();
+                        oos.close();
+                        ois.close();
+                        addressSocket.close();
+                        break;
+                    }
+                    oos.flush();
                 }
-                oos.flush();
-            }
-        } catch (ClassNotFoundException e) {
+            } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.err.println("Error reading object from server: " + e.getMessage());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 
     public static void main(String[] args) {
         new Client();
